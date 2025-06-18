cd23f38630f692a4d5f088a97da09555df9f6c82
volkovlev <145903224+volkov-lev@users.noreply.github.com>
2025-06-18 18:49:24 +0300

    Update trafficStats.js

--- generate-cards/trafficStats.js
+++ generate-cards/trafficStats.js
@@ -183,6 +183,17 @@ function generateSVG(repoStats) {
       stroke: ${colors.dark.stroke};
     }
 
+    .header-outline {
+      fill: none;
+      stroke: ${colors.light.stroke}; 
+      stroke-width: 0.7px;
+      rx: 5px;
+      ry: 5px;
+    }
+    #gh-dark-mode-only:target .header-outline {
+      stroke: ${colors.dark.stroke};
+    }
+
     .title-cards {
       font-size: 15px; 
       font-weight: bold;
@@ -241,7 +252,7 @@ function generateSVG(repoStats) {
       font-size: 12px;
       font-weight: 600;
       color: ${colors.light.repositoryText};
-      line-height: 1.5; 
+      line-height: 1.6; 
     }
 
     #gh-dark-mode-only:target .repo-name {
@@ -281,14 +292,19 @@ function generateSVG(repoStats) {
   <g>
     <rect id="background" x="0" y="0" width="385" height="180" />
     
+    <rect class="header-outline" x="3" y="3" width="379" height="35" />
     <g transform="translate(16, 15)">
       <svg class="github-icon" width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
       </svg>
-      <text class="title-cards" x="22" y="13" font-family="Arial">Top 5 Repositories by Traffic (past two weeks)</text>
+      <text class="title-cards" x="22" y="10" font-family="Arial">Top 5 Repositories by Traffic (past two weeks)</text>
     </g>
 
-    <foreignObject x="5" y="40" width="365" height="140">
+    <rect class="header-outline" x="3" y="41" width="141" height="136" />
+    <rect class="header-outline" x="147" y="41" width="116" height="136" />
+    <rect class="header-outline" x="266" y="41" width="116" height="136" />
+    
+    <foreignObject x="16" y="45" width="353" height="27">
       <table xmlns="http://www.w3.org/1999/xhtml" style="width: 100%; border-collapse: collapse;">
         <thead>
           <tr>
@@ -306,6 +322,151 @@ function generateSVG(repoStats) {
 </svg>
 `;
 }
+
+
+//   return `
+// <svg id="gh-dark-mode-only" width="385" height="180" xmlns="http://www.w3.org/2000/svg">
+//   <style>
+//     svg {
+//       font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji;
+//     }
+
+//     #background {
+//       fill: ${colors.light.background};
+//       stroke: ${colors.light.stroke};
+//       stroke-width: 1.7px;
+//       rx: 6px;
+//       ry: 6px;
+//     }
+
+//     #gh-dark-mode-only:target #background {
+//       fill: ${colors.dark.background};
+//       stroke: ${colors.dark.stroke};
+//     }
+
+//     .title-cards {
+//       font-size: 15px; 
+//       font-weight: bold;
+//       fill: ${colors.light.titleCards}; 
+//     }
+
+//     #gh-dark-mode-only:target .title-cards {
+//       fill: ${colors.dark.titleCards};
+//     }
+
+//     .github-icon path {
+//       fill: ${colors.light.iconGithub};
+//     }
+
+//     #gh-dark-mode-only:target .github-icon path {
+//       fill: ${colors.dark.iconGithub};
+//     }
+
+//     th {
+//       font-size: 11.5px;
+//       font-weight: 600;
+//       color: ${colors.light.textTitle};
+//       text-shadow: 
+//         1px 1px 0px ${colors.light.borderColor}, 
+//         -1px -1px 0px ${colors.light.borderColor}, 
+//         1px -1px 0px ${colors.light.borderColor}, 
+//         -1px 1px 0px ${colors.light.borderColor};
+//       padding-bottom: 5px;
+//     }
+
+//     #gh-dark-mode-only:target th {
+//       color: ${colors.dark.textTitle};
+//       text-shadow: 
+//         0.7px 0.7px 0px ${colors.dark.borderColor}, 
+//         -0.7px -0.7px 0px ${colors.dark.borderColor}, 
+//         0.7px -0.7px 0px ${colors.dark.borderColor}, 
+//         -0.7px 0.7px 0px ${colors.dark.borderColor};
+//     }
+
+//     .folder-icons {
+//       vertical-align: middle; 
+//     }
+
+//     .folder-icons path {
+//       fill: ${colors.light.folderIcons}; 
+//       stroke: ${colors.light.folderIconOutline}; 
+//       stroke-width: 1; 
+//     }
+
+//     #gh-dark-mode-only:target .folder-icons path {
+//       fill: ${colors.dark.folderIcons}; 
+//       stroke: ${colors.dark.folderIconOutline}; 
+//     }
+
+//     .repo-name {
+//       font-size: 12px;
+//       font-weight: 600;
+//       color: ${colors.light.repositoryText};
+//       line-height: 1.5; 
+//     }
+
+//     #gh-dark-mode-only:target .repo-name {
+//       color: ${colors.dark.repositoryText};
+//     }
+
+//     .unique-count {
+//       font-size: 11.5px;
+//       font-weight: 600;
+//       color: ${colors.light.uniqueCount};
+//     }
+
+//     #gh-dark-mode-only:target .unique-count {
+//       color: ${colors.dark.uniqueCount};
+//     }
+
+//     .date-range {
+//       font-size: 11.5px;
+//       font-weight: 600; 
+//       color: ${colors.light.dateRange};
+//     }
+
+//     #gh-dark-mode-only:target .date-range {
+//       color: ${colors.dark.dateRange};
+//     }
+
+//     @keyframes fadein {
+//       0% { opacity: 0; }
+//       100% { opacity: 1; }
+//     }
+
+//     .repo-row {
+//       opacity: 0;
+//       animation: fadein 0.5s ease-in forwards;
+//     }
+//   </style>
+//   <g>
+//     <rect id="background" x="0" y="0" width="385" height="180" />
+    
+//     <g transform="translate(16, 15)">
+//       <svg class="github-icon" width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+//         <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+//       </svg>
+//       <text class="title-cards" x="22" y="13" font-family="Arial">Top 5 Repositories by Traffic (past two weeks)</text>
+//     </g>
+
+//     <foreignObject x="5" y="40" width="365" height="140">
+//       <table xmlns="http://www.w3.org/1999/xhtml" style="width: 100%; border-collapse: collapse;">
+//         <thead>
+//           <tr>
+//             <th style="width: ${repoColumnWidth}px; text-align: left; padding-left: 35px;">Repository</th>
+//             <th style="width: ${uniquesColumnWidth}px; text-align: center; padding-right: 30px;">Uniques</th>
+//             <th style="width: ${dateColumnWidth}px; text-align: center;">Date Range</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           ${rowsHTML}
+//         </tbody>
+//       </table>
+//     </foreignObject>
+//   </g>
+// </svg>
+// `;
+// }
 
 async function main() {
   try {
