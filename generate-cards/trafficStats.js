72186da8d2082f93d2f5029586bd82ffeeefbe88
volkovlev <145903224+volkov-lev@users.noreply.github.com>
2025-06-19 17:47:20 +0300

    Update trafficStats.js

--- generate-cards/trafficStats.js
+++ generate-cards/trafficStats.js
@@ -21,13 +21,13 @@ const colors = {
     borderColor: "rgb(88, 96, 105)", // Border color of statistics column headers (Цвет обовдки заголовков столбцов статистики)
     folderIcons: "rgb(88, 96, 105)", // Folder Icon Fill Color (Цвет заливки иконок папок)
     folderIconOutline: "rgb(88, 96, 105)", // Folder Icon Outline Color (Цвет обводки иконок папок)
-    repositoryText: "#000000", // Repository name color (Цвет названия репозитория)
-    uniqueCount: "#000000", // Color of number of visitors (Цвет кол-ва посетителей)
-    dateRange: "#000000", // Date range color (Цвет диапазона дат)
+    repositoryText: "rgb(88, 96, 105)", // Repository name color (Цвет названия репозитория)
+    uniqueCount: "rgb(88, 96, 105)", // Color of number of visitors (Цвет кол-ва посетителей)
+    dateRange: "rgb(88, 96, 105)", // Date range color (Цвет диапазона дат)
   },
   dark: {
     background: "none",
-    stroke: "rgb(225, 228, 232)",
+    stroke: "rgb(225, 228, 232, 0.5)",
     iconGithub: "#8b949e",
     titleCards: "#006AFF",
     textTitle: "#000000",
@@ -217,20 +217,20 @@ function generateSVG(repoStats) {
       font-weight: 600;
       color: ${colors.light.textTitle};
       text-shadow: 
-        1px 1px 0px ${colors.light.borderColor}, 
-        -1px -1px 0px ${colors.light.borderColor}, 
-        1px -1px 0px ${colors.light.borderColor}, 
-        -1px 1px 0px ${colors.light.borderColor};
-      padding-bottom: 5px;
+        0.5px 0.5px 0px ${colors.light.borderColor}, 
+        -0.5px -0.5px 0px ${colors.light.borderColor}, 
+        0.5px -0.5px 0px ${colors.light.borderColor}, 
+        -0.5px 0.5px 0px ${colors.light.borderColor};
+      padding-bottom: 10px;
     }
 
     #gh-dark-mode-only:target th {
       color: ${colors.dark.textTitle};
       text-shadow: 
-        0.7px 0.7px 0px ${colors.dark.borderColor}, 
-        -0.7px -0.7px 0px ${colors.dark.borderColor}, 
-        0.7px -0.7px 0px ${colors.dark.borderColor}, 
-        -0.7px 0.7px 0px ${colors.dark.borderColor};
+        0.5px 0.5px 0px ${colors.dark.borderColor}, 
+        -0.5px -0.5px 0px ${colors.dark.borderColor}, 
+        0.5px -0.5px 0px ${colors.dark.borderColor}, 
+        -0.5px 0.5px 0px ${colors.dark.borderColor};
     }
 
     .folder-icons {
@@ -250,7 +250,6 @@ function generateSVG(repoStats) {
 
     .repo-name {
       font-size: 12px;
-      font-weight: 600;
       color: ${colors.light.repositoryText};
       line-height: 1.6; 
     }
@@ -260,8 +259,7 @@ function generateSVG(repoStats) {
     }
 
     .unique-count {
-      font-size: 11.5px;
-      font-weight: 600;
+      font-size: 12px;
       color: ${colors.light.uniqueCount};
     }
 
@@ -270,8 +268,7 @@ function generateSVG(repoStats) {
     }
 
     .date-range {
-      font-size: 11.5px;
-      font-weight: 600; 
+      font-size: 12px;
       color: ${colors.light.dateRange};
     }
 
@@ -294,17 +291,21 @@ function generateSVG(repoStats) {
     
     <rect class="header-outline" x="3" y="3" width="379" height="35" />
     <g transform="translate(16, 15)">
-      <svg class="github-icon" width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+      <svg class="github-icon" x="-2" y="-4" width="18" height="18" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
       </svg>
       <text class="title-cards" x="22" y="10" font-family="Arial">Top 5 Repositories by Traffic (past two weeks)</text>
     </g>
 
-    <rect class="header-outline" x="3" y="41" width="141" height="136" />
-    <rect class="header-outline" x="147" y="41" width="116" height="136" />
-    <rect class="header-outline" x="266" y="41" width="116" height="136" />
+    <rect class="header-outline" x="3" y="41" width="141" height="20" />
+    <rect class="header-outline" x="147" y="41" width="116" height="20" />
+    <rect class="header-outline" x="266" y="41" width="116" height="20" />
     
-    <foreignObject x="0" y="45" width="380" height="120">
+    <rect class="header-outline" x="3" y="64" width="141" height="112" />
+    <rect class="header-outline" x="147" y="64" width="116" height="112" />
+    <rect class="header-outline" x="266" y="64" width="116" height="112" />
+    
+    <foreignObject x="0" y="43" width="380" height="140">
       <table xmlns="http://www.w3.org/1999/xhtml" style="width: 100%; border-collapse: collapse;">
         <thead>
           <tr>
